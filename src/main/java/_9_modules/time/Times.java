a677b89fa0e84593074d2a994a9dae0a7751dbba
ahrooran-r <ahrooran17@gmail.com>
2022-05-23 17:25:21 +0530

    date time package

---
+++ src/main/java/_9_modules/time/Times.java
@@ -0,0 +1,11 @@
+package _9_modules.time;
+
+import java.time.LocalTime;
+
+public class Times {
+    public static void main(String[] args) {
+
+        LocalTime lt = LocalTime.of(14, 28, 34, 300);
+
+    }
+}
