13ed1c58294a6028e4f9584a556431782af7d853
ahrooran-r <ahrooran17@gmail.com>
2022-06-02 18:16:09 +0530

    switched to module based structure. added guava tutorials

---
+++ guava/src/main/java/_11_guava/_2_collections/_2_SetsClass.java
@@ -0,0 +1,33 @@
+package _11_guava._2_collections;
+
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <a href="https://www.baeldung.com/guava-sets">tutorial</a>
+ */
+public class _2_SetsClass {
+    public static void main(String[] args) {
+
+        Set<String> set1 = Sets.newHashSet("John", "Jane", "Adam");
+        Set<String> set2 = Sets.newHashSet("Tom", "Viki", null, "Tyler");
+
+        // union of set
+        Set<String> union = Sets.union(set1, set2);
+
+        // cartesian product
+        // https://www.baeldung.com/java-generics
+        Set<List<String>> cartesianProduct = Sets.cartesianProduct(set1, set2);
+
+        // intersection
+        Set<String> intersection = Sets.intersection(set1, set2);
+
+        // symmetric difference -> all elements that are contained in either set 1 or set 2 but not in both:
+        Set<String> symmetricDifference = Sets.symmetricDifference(set1, set2);
+
+        // power set –> the set of all possible subsets of that set.
+        Set<Set<String>> powerSet = Sets.powerSet(union);
+    }
+}
---
+++ guava/src/main/java/_11_guava/_2_collections/_3_ImmutableMapClass.java
@@ -0,0 +1,30 @@
+package _11_guava._2_collections;
+
+import com.google.common.collect.*;
+
+import java.util.Map;
+
+/**
+ * <a href="https://www.baeldung.com/guava-maps">tutorial</a>
+ */
+public class _3_ImmutableMapClass {
+    public static void main(String[] args) {
+
+        // building an immutable map
+        Map<String, Integer> salary = ImmutableMap.<String, Integer>builder()
+                .put("John", 1000)
+                .put("Jane", 1500)
+                .put("Adam", 2000)
+                .put("Tom", 2000)
+                .build();
+
+        // creating a sorted map according to ordering
+        ImmutableSortedMap<String, Integer> salary2 = new ImmutableSortedMap
+                .Builder<String, Integer>(Ordering.natural())
+                .put("John", 1000)
+                .put("Jane", 1500)
+                .put("Adam", 2000)
+                .put("Tom", 2000)
+                .build();
+    }
+}
