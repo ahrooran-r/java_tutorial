36f7da4067c8f9f15503255df63b9e30ef837977
ahrooran-r <ahrooran17@gmail.com>
2022-10-21 08:49:21 +0530

    compilers and tuning

---
+++ performance_and_memory_management/src/main/java/tutorial/learn/performance_and_memory_management/_2_prime_example/Main.java
@@ -0,0 +1,18 @@
+package tutorial.learn.performance_and_memory_management._2_prime_example;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        Integer max = Integer.parseInt(args[0]);
+        primeNumbers.generateNumbers(max);
+
+        System.out.println("time = " + (System.currentTimeMillis() - start) + " ms");
+
+        // 1131 ms
+    }
+
+}
