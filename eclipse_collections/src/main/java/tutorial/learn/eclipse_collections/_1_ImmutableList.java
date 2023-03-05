1e18ed426255d255fbac54a5684c72e37d03b076
AhrooranR <r.ahrooran@gtngroup.com>
2023-03-05 22:07:31 +0530

    eclipse collections

---
+++ eclipse_collections/src/main/java/tutorial/learn/eclipse_collections/_1_ImmutableList.java
@@ -0,0 +1,12 @@
+package tutorial.learn.eclipse_collections;
+
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.list.mutable.FastList;
+
+public class _1_ImmutableList {
+    public static void main(String[] args) {
+
+        MutableList<String> ml = FastList.newList();
+
+    }
+}
