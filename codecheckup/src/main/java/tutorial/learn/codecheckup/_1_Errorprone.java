fadd4c91d698b7886fa3bda066b089c26b0700c6
ahrooran-r <Ahroo17@github>
2023-03-19 00:10:51 +0530

    1. commented out some modules 2. migrated to java 19 3. centralized dependency versions 4. updated dependency versions

--- codecheckup/src/main/java/tutorial/learn/codecheckup/_1_Errorprone.java
+++ codecheckup/src/main/java/tutorial/learn/codecheckup/_1_Errorprone.java
@@ -1,25 +1,25 @@
-package tutorial.learn.codecheckup;
-
-import lombok.Getter;
-
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- * When `mvn clean compile` error-prone should show bugs here
- * <a href="https://errorprone.info/api/latest/com/google/errorprone/annotations/package-summary.html">annotation summary</a>
- */
-@SuppressWarnings("SameNameButDifferent")
-public class _1_Errorprone {
-
-    @Getter
-    private static final Set<Short> s = new HashSet<>();
-
-    public static void main(String[] args) {
-        for (short i = 0; i < 100; i++) {
-            getS().add(i);
-            getS().remove(i - 1);
-        }
-        System.out.println(s.size());
-    }
-}
+// package tutorial.learn.codecheckup;
+//
+// import lombok.Getter;
+//
+// import java.util.HashSet;
+// import java.util.Set;
+//
+// /**
+//  * When `mvn clean compile` error-prone should show bugs here
+//  * <a href="https://errorprone.info/api/latest/com/google/errorprone/annotations/package-summary.html">annotation summary</a>
+//  */
+// @SuppressWarnings("SameNameButDifferent")
+// public class _1_Errorprone {
+//
+//     @Getter
+//     private static final Set<Short> s = new HashSet<>();
+//
+//     public static void main(String[] args) {
+//         for (short i = 0; i < 100; i++) {
+//             getS().add(i);
+//             getS().remove(i - 1);
+//         }
+//         System.out.println(s.size());
+//     }
+// }
