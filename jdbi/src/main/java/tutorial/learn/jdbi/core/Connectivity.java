e790c07aa8ccfddae3b9071efb5a97e303eb6d1b
ahrooran-r <Ahroo17@github>
2023-06-04 17:16:23 +0530

    enabled java 20 preview features

--- jdbi/src/main/java/tutorial/learn/jdbi/core/Connectivity.java
+++ jdbi/src/main/java/tutorial/learn/jdbi/core/Connectivity.java
@@ -1,13 +1,15 @@
 package tutorial.learn.jdbi.core;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jdbi.v3.core.Jdbi;
 
 @Slf4j
 public class Connectivity {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306", "root", "password");
-        log.info(jdbi.toString());
+        // Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306", "root", "password");
+        // log.info(jdbi.toString());
+
+        Thread t = Thread.ofVirtual().start(() -> System.out.println("Hello World"));
+        t.join();
     }
 }
